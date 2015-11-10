4b70e47ce332aa855754e043497303a8cdcd8a84
Jia Wei <jwei@linkedin.com>
2015-11-10 18:11:46 +0800

    1.add example for Gin framework;2.modify example for Martini.

--- examples/martini/server.go
+++ examples/martini/server.go
@@ -7,7 +7,7 @@ import (
 )
 
 func main() {
-	yaag.Init(&yaag.Config{On: true, DocTitle: "Gorilla Mux", DocPath: "apidoc.html", BaseUrls: map[string]string{"Production": "", "Staging": ""}})
+	yaag.Init(&yaag.Config{On: true, DocTitle: "Martini", DocPath: "apidoc.html", BaseUrls: map[string]string{"Production": "", "Staging": ""}})
 	m := martini.Classic()
 	m.Use(martiniyaag.Document)
 	m.Get("/", func() string {
