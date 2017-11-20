41ca6cf90d21863e8f5e68eeaf012a35dcff70aa
Aniket Awati <aniket@media.net>
2017-11-20 13:27:53 +0530

    add negroni example #45

---
+++ examples/negroni/server.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"github.com/betacraft/yaag/yaag"
+	"github.com/gorilla/mux"
+	"net/http"
+	"github.com/betacraft/yaag/middleware"
+	"fmt"
+	"time"
+	"github.com/urfave/negroni"
+)
+
+func main() {
+	yaag.Init(&yaag.Config{On: true, DocTitle: "Negroni-gorilla", DocPath: "apidoc.html", BaseUrls: map[string]string{"Production": "", "Staging": ""}})
+
+	router := mux.NewRouter()
+
+	router.HandleFunc("/", middleware.HandleFunc(handler))
+	n := negroni.Classic()
+	n.UseHandler(router)
+	n.Run(":5000")
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, time.Now().String())
+}
