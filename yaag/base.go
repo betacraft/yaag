adb2276bbb4dfd74eddb3da92a37e7d5b9b624a6
AN <anbrsy@gmail.com>
2019-04-17 23:41:33 +0800

    China not load js, so it is suggested to change
    China blocked Google can not load js, so it is suggested to change
    ...China GWF...

--- yaag/base.go
+++ yaag/base.go
@@ -8,7 +8,7 @@ const Template = `<!DOCTYPE html>
     <link href='http://fonts.googleapis.com/css?family=Roboto' rel='stylesheet' type='text/css'>
     <!-- Optional theme -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap-theme.min.css">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+    <script src="http://code.jquery.com/jquery-2.1.3.min.js"></script>   
     <!-- Latest compiled and minified JavaScript -->
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/js/bootstrap.min.js"></script>
     <style type="text/css">
